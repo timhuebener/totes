4bfc5cdaa9ef2167c0c5d47093d81d55d3ba5722
Tim Hübener <tim.huebener@gmail.com>
2023-05-05 23:55:34 +0200

    feat: make PWA

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/routes';
 import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
     importProvidersFrom(BrowserAnimationsModule, MatSnackBarModule),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 });
