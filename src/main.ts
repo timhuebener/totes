fa90100e6a71e2c2492b9dc1eb27d81b6a3670a9
Tim Hübener <tim.huebener@gmail.com>
2023-05-05 09:24:25 +0200

    feat: use github flavoured markdown

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/routes';
 import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
-import { MarkdownModule } from 'ngx-markdown';
+import { SecurityContext } from '@angular/core';
+
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
-    importProvidersFrom(BrowserAnimationsModule, MarkdownModule.forRoot()),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MarkdownModule.forRoot({
+        sanitize: SecurityContext.NONE,
+        markedOptions: {
+          provide: MarkedOptions,
+          useValue: {
+            gfm: true,
+          },
+        },
+      })
+    ),
   ],
 });
